d36625000150e6bc7e233a6459edcf22047db592
g-bolsoni <giovanebolsoni2@gmail.com>
2020-08-28 12:50:32 -0300

    Project on making a list

---
+++ SmallProjects/to_do_list/src/components/TodoList/index.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import TodoItens from '../TodoItens' 
+class TodoList extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state={
+            tarefa:'',
+            itens:[]
+        }
+
+        this.addItem = this.addItem.bind(this);
+        this.log = this.log.bind(this);
+        this.deleteItem = this.deleteItem.bind(this)
+    }
+    addItem(e){
+        let state =this.state
+        if(this._tarefaInput.value !== ''){
+            let newItem={
+                text: this._tarefaInput.value,
+                key: Date.now()
+            };
+            this.setState({itens:[...state.itens, newItem]});
+        }  
+    
+        e.preventDefault();
+        this.setState({tarefa:''})
+    }
+
+    log(){
+        
+        console.log(this.state.itens)
+    }
+
+    deleteItem(key){
+        console.log(`Item a ser deletado ${key}`)
+        let filtro = this.state.itens.filter((item)=>{
+            return (item.key !== key)
+        })
+        this.setState({itens:filtro})
+    }
+  render(){
+    return (
+      <div >
+          <h3>Componente TodoList</h3>
+          <form onSubmit={this.addItem}>
+              <input type="text" placeholder="Nova Tarefa ?" name="tarefa" 
+                     value={this.state.tarefa} 
+                     onChange={(e)=>this.setState({tarefa: e.target.value})}
+                     ref={(e)=>{this._tarefaInput = e}} />
+
+                <button type="submit"> ADD</button>
+          </form>
+
+          <button onClick={this.log}>LOG</button>
+
+          <TodoItens lista={this.state.itens} delete={this.deleteItem}/>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
